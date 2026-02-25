373e13864b0122156598836d5f64134e2e868e13
IDGS-801-23001497 <zac.universe.utl@gmail.com>
2026-02-25 17:59:15 -0600

    abc completo 25/02/2026

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from re import M
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
@@ -5,11 +7,14 @@ from flask import g
 
 from config import DevelopmentConfig
 import forms
+from flask_migrate import Migrate, migrate
 from models import db, Alumnos
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf=CSRFProtect()
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 @app.route("/index")
@@ -68,9 +73,29 @@ def editar():
 		return redirect(url_for('index'))
 	return render_template('editar.html',form=create_form)
 
+@app.route('/eliminar', methods=['GET','POST'])
+def eliminar():
+	create_form=forms.UserForm2(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		if alum1:
+			create_form.id.data=alum1.id
+			create_form.nombre.data=alum1.nombre
+			create_form.apaterno.data=alum1.apaterno
+			create_form.email.data=alum1.email
+			return render_template("eliminar.html", form=create_form)
+	if request.method == 'POST':
+		id = create_form.id.data
+		alum = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		if alum:
+			db.session.delete(alum)
+			db.session.commit()
+		return redirect(url_for('index'))
+ 
+
 if __name__ == '__main__':
 	csrf.init_app(app)
-	db.init_app(app)
 	with app.app_context():
 		db.create_all()
 	app.run(debug=True)
